269b78f6f517ac6f638cc4052b009af3b423f49e
Rocketmayn <33028095+Rocketmayn@users.noreply.github.com>
2018-06-04 20:31:05 +0200

    test
    test

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
   <body>
     <?php include("php/header.php"); ?>
-    <?php include('../Examen2018/includes/content.html') ?>
+    <?php include('includes/content.html') ?>
     <h2 class="text-center pt-4">Welkom</h2>
     <section class="p-5 mr-2">
       <div class="textbox-main col12">
@@ -51,87 +51,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <p>Alle bronnen, links en referenties worden opgeslagen in een document dat hier op de website te zien zal zijn d.m.v deze link: <a href="#">Bronnen</a></p>
     </section>
 
-    <div class="form">
-
-        <ul class="tab-group">
-          <li class="tab"><a href="#signup">Sign Up</a></li>
-          <li class="tab active"><a href="#login">Log In</a></li>
-        </ul>
-
-        <div class="tab-content">
-
-           <div id="login">
-            <h1>Welcome Back!</h1>
-
-            <form action="index.php" method="post" autocomplete="off">
-
-              <div class="field-wrap">
-              <label>
-                Email Address<span class="req">*</span>
-              </label>
-              <input type="email" required autocomplete="off" name="email"/>
-            </div>
-
-            <div class="field-wrap">
-              <label>
-                Password<span class="req">*</span>
-              </label>
-              <input type="password" required autocomplete="off" name="password"/>
-            </div>
-
-            <p class="forgot"><a href="forgot.php">Forgot Password?</a></p>
-
-            <button class="button button-block" name="login" />Log In</button>
-
-            </form>
-
-          </div>
-
-          <div id="signup">
-            <h1>Sign Up for Free</h1>
-
-            <form action="index.php" method="post" autocomplete="off">
-
-            <div class="top-row">
-              <div class="field-wrap">
-                <label>
-                  First Name<span class="req">*</span>
-                </label>
-                <input type="text" required autocomplete="off" name='firstname' />
-              </div>
-
-              <div class="field-wrap">
-                <label>
-                  Last Name<span class="req">*</span>
-                </label>
-                <input type="text"required autocomplete="off" name='lastname' />
-              </div>
-            </div>
-
-            <div class="field-wrap">
-              <label>
-                Email Address<span class="req">*</span>
-              </label>
-              <input type="email"required autocomplete="off" name='email' />
-            </div>
-
-            <div class="field-wrap">
-              <label>
-                Set A Password<span class="req">*</span>
-              </label>
-              <input type="password"required autocomplete="off" name='password'/>
-            </div>
-
-            <button type="submit" class="button button-block" name="register" />Register</button>
-
-            </form>
-
-          </div>
-
-        </div>
-      </div>
-
-    <?php include('../Examen2018/includes/footer.html') ?>
+    <?php include('includes/footer.html') ?>
     <div class="content">
       <!-- notification message -->
       <?php if (isset($_SESSION['success'])) : ?>
