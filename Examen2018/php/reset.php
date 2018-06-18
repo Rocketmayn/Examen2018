b96b42b91fcfafee389994c5fd08423350203202
Angelo <Angelo@iclicks.nl>
2018-06-18 14:17:39 +0200

    ff poesen
    poes

--- Examen2018/php/reset.php
+++ Examen2018/php/reset.php
@@ -1,28 +1,28 @@
 <?php
-/* 
+/*
 Het wachtwoordherstelformulier, de link naar deze pagina is inbegrepen in het emailbericht forgot.php
 */
 require 'db.php';
 session_start();
 
-// Zorg dat mail en hash variabelen niet leeg zijn 
+// Zorg dat mail en hash variabelen niet leeg zijn
 if( isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash']) )
 {
-    $email = $mysqli->escape_string($_GET['email']); 
-    $hash = $mysqli->escape_string($_GET['hash']); 
+    $email = $mysqli->escape_string($_GET['email']);
+    $hash = $mysqli->escape_string($_GET['hash']);
 
     // Zorg dat mail met matchende hash al wel bestaat
     $result = $mysqli->query("SELECT * FROM users WHERE email='$email' AND hash='$hash'");
 
     if ( $result->num_rows == 0 )
-    { 
+    {
         $_SESSION['message'] = "Verkeerde URL ingevoerd om het wachtwoord te resetten!";
         header("location: error.php");
     }
 }
 else {
     $_SESSION['message'] = "Verificatie mislukt, probeer het opnieuw!";
-    header("location: error.php");  
+    header("location: error.php");
 }
 ?>
 <!DOCTYPE html>
@@ -30,36 +30,36 @@ else {
 <head>
   <meta charset="UTF-8">
   <title>Reset je wachtwoord</title>
-  <?php include 'css/css.html'; ?>
+  <link rel="stylesheet" href="../style/algemeneStyle.css">
 </head>
 
 <body>
     <div class="form">
 
           <h1>Kies je nieuwe wachtword</h1>
-          
+
           <form action="reset_password.php" method="post">
-              
+
           <div class="field-wrap">
             <label>
               Nieuwe wachtwoord<span class="req">*</span>
             </label>
             <input type="password"required name="newpassword" autocomplete="off"/>
           </div>
-              
+
           <div class="field-wrap">
             <label>
               Bevestig nieuwe wachtwoord<span class="req">*</span>
             </label>
             <input type="password"required name="confirmpassword" autocomplete="off"/>
           </div>
-          
+
           <!-- Dit veld is verplicht, zo krijg je het e-mailadres van de gebruiker -->
-          <input type="hidden" name="email" value="<?= $email ?>">    
-          <input type="hidden" name="hash" value="<?= $hash ?>">    
-              
+          <input type="hidden" name="email" value="<?= $email ?>">
+          <input type="hidden" name="hash" value="<?= $hash ?>">
+
           <button class="button button-block"/>Toepassen</button>
-          
+
           </form>
 
     </div>
